1a7b7690321cf4981a2e16584d28d08dda199c40
Kiselev <kiselek777099@gmail.com>
2024-12-21 12:15:49 +0300

    Мой гардероб

---
+++ app/src/main/java/com/example/mywardrobe/WardrobeActivity.kt
@@ -0,0 +1,76 @@
+package com.example.mywardrobe
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class WardrobeActivity : AppCompatActivity() {
+    lateinit var wardrobeRV:RecyclerView
+    lateinit var mainTB:Toolbar
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_wardrobe)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        mainTB=findViewById(R.id.mainTB)
+        setSupportActionBar(mainTB)
+        title="Мой гардероб"
+        wardrobeRV = findViewById(R.id.wardrobeRV)
+        wardrobeRV.layoutManager = LinearLayoutManager(this)
+
+        val items = getWardrobeItems()
+        val adapter = WardrobeAdapter(items)
+        wardrobeRV.adapter = adapter
+    }
+    private fun getWardrobeItems(): List<WardrobeItem> {
+        return listOf(
+            WardrobeItem("Футболка", "Удобная летняя футболка", R.drawable.tshirt),
+            WardrobeItem("Джинсы", "Классические джинсы", R.drawable.jeans),
+            WardrobeItem("Куртка", "Теплая зимняя куртка", R.drawable.kurtka),
+            WardrobeItem("Носки", "Практичные носки", R.drawable.noski),
+            WardrobeItem("Туфли", "Удобные туфли", R.drawable.tufli),
+            WardrobeItem("Шуба", "Меховая шуба", R.drawable.shooba),
+            WardrobeItem("Рубашка", "Классическая рубашка", R.drawable.rubashka),
+            WardrobeItem("Платье", "Вечернее платье", R.drawable.plate),
+            WardrobeItem("Сапоги", "Осенние сапоги", R.drawable.sapogi),
+            WardrobeItem("Костюм", "Классический костюм", R.drawable.kostum),
+            WardrobeItem("Шапка", "Теплая шапка", R.drawable.shapka),
+            WardrobeItem("Плавки", "Удобные плавки", R.drawable.plavki),
+            WardrobeItem("Футболка", "Удобная летняя футболка", R.drawable.tshirt),
+            WardrobeItem("Джинсы", "Классические джинсы", R.drawable.jeans),
+            WardrobeItem("Футболка", "Удобная летняя футболка", R.drawable.tshirt),
+            WardrobeItem("Джинсы", "Классические джинсы", R.drawable.jeans),
+            WardrobeItem("Шуба", "Меховая шуба", R.drawable.shooba),
+            WardrobeItem("Рубашка", "Классическая рубашка", R.drawable.rubashka),
+            WardrobeItem("Платье", "Вечернее платье", R.drawable.plate),
+            WardrobeItem("Сапоги", "Осенние сапоги", R.drawable.sapogi)
+        )
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.exitApp -> {
+                finishAffinity()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mywardrobe/WardrobeItem.kt
@@ -0,0 +1,3 @@
+package com.example.mywardrobe
+
+data class WardrobeItem(val name: String, val description: String, val imageResId: Int)
